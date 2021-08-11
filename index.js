455655a0ea9bdee3c61a7e83f7206701fc3faeae
Allan Verde <allangreen2906@gmail.com>
2021-08-11 09:14:45 -0300

    v1.3

--- index.js
+++ index.js
@@ -1,80 +1,57 @@
 function snapCrackle(maxValue) {
     let res = ''
-    
-    for (let i = 0; i < maxValue; i++) {    
-        if ((Number.isInteger(i % 5) === true) && i % 2 !== 0) {
-            res = res + 'SnapCrackle, '
-        } else if (i % 2 !== 0) {
-            res = res + 'Snap, '
-        } else if (Number.isInteger(i % 5) == true) {
-            res = res + 'Crackle, '
+    for (i = 1; i <= maxValue; i++) {
+        if (i % 2 !== 0) {
+            res += 'Snap'
+        } 
+        if (i % 5 === 0) {
+            res += 'Crackle'
+        }
+        if (i % 2 === 0 && i % 5 !== 0) {
+            res += i
+        }
+        if (i < maxValue) {
+            res += ', '
         } else {
-            res = res + i + ', '
+            res += '.'
+            break
         }
     }
-
-    if (i = maxValue) {    
-        if ((Number.isInteger(i % 5) === true) && i % 2 !== 0) {
-            res = res + 'SnapCrackle.'
-        } else if (i % 2 !== 0) {
-            res = res + 'Snap.'
-        } else if (Number.isInteger(i % 5) == true) {
-            res = res + 'Crackle.'
-        } else {
-            res = res + i + '.'
-        }
-    }
-    return res
+return res
 }
-
+// ----------------------------------------------------------
 function snapCracklePrime(maxValue) {
     let res = ''
-    let i = 1
-    for (; i < maxValue; i++) {
-        ehPrimo = false
-        for (let divisor = 2; divisor < i; divisor++) {
-            if (i % divisor !== 0) {
-                ehPrimo = true
-                break
+    let ehPrimo = true
+    for (let i = 1; i <= maxValue; i++) {
+        if (i % 2 !== 0) {
+            res += 'Snap'
+        } 
+
+        if (i % 5 === 0) {
+            res += 'Crackle'
+        }
+
+        for (divisor = 2; divisor < i; divisor++) {
+            if (i % divisor === 0) {
+                ehPrimo = false
             }
         }
-        if (Number.isInteger(i % 5) === true && i % 2 !== 0 && ehPrimo === true && i !== 1, 2) {
-            res = res + 'SnapcracklePrime, '
-        } else if ((Number.isInteger(i % 5) === true) && i % 2 !== 0) {
-            res = res + 'SnapCrackle, '
-        } else if (i % 2 !== 0 && ehPrimo === true) {
-            res = res + 'SnapPrime, '
-        } else if (i % 2 !== 0) {
-            res = res + 'Snap, '
-        } else if (Number.isInteger(i % 5) === true && ehPrimo === true) {
-            res + res + 'CracklePrime, '
-        } else if (Number.isInteger(i % 5) === true) {
-            res = res + 'Crackle, '
-        } else if (ehPrimo === true) {
-            res = res + 'Prime, '
-        } else {
-            res = res + i + ', '
-        }
-    }
 
-    if (i = maxValue) {    
-        if ((Number.isInteger(i % 5) === true) && i % 2 !==0 && ehPrimo === true && i !== 1 && i !== 2) {
-            res = res + 'SnapcracklePrime.'
-        } else if ((Number.isInteger(i % 5) === true) && (i % 2 !== 0) && i !== 1) {
-            res = res + 'SnapCrackle.'
-        } else if (i % 2 !== 0 && ehPrimo === true) {
-            res = res + 'SnapPrime.'
-        } else if (i % 2 !== 0) {
-            res = res + 'Snap.'
-        } else if (Number.isInteger(i % 5) === true && ehPrimo === true) {
-            res + res + 'CracklePrime.'
-        } else if (Number.isInteger(i % 5) === true) {
-            res = res + 'Crackle.'
-        } else if (ehPrimo === true) {
-            res = res + 'Prime.'
+        if (ehPrimo === true) {
+            res += 'Prime'
+        }
+
+        if (i % 2 === 0 && i % 5 !== 0 && ehPrimo !== true) {
+            res += i
+        }
+
+        if (i < maxValue) {
+            res += ', '
         } else {
-            res = res + i + '.'
+            res += '.'
+            break
         }
     }
-    return res
+return res
 }
